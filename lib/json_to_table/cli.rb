c50a68b93018e4a8641f91b1866347c93010fe0e
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2022-04-03 15:52:28 +0900

    jsomのキーをヘッダーとして表示するロジック追加

--- lib/json_to_table/cli.rb
+++ lib/json_to_table/cli.rb
@@ -5,7 +5,7 @@ module JsonToTable
   class CLI < Thor
     desc 'show', 'show json as table, [argument] file_name'
     def show(json_file)
-      JsonToTableShower.new(File.read(json_file)).show
+      JsonToTableShower.new(File.read(json_file)).run
     end
   end
 end
--- lib/json_to_table_shower.rb
+++ lib/json_to_table_shower.rb
@@ -1,11 +1,26 @@
 require 'json'
+require 'terminal-table'
 
 class JsonToTableShower
   def initialize(json)
     @json = JSON.parse(json)
   end
 
-  def show
-    puts @json
+  # arrayが来る前提の処理
+  def run
+    rows = []
+    table = Terminal::Table.new headings: headings(@json), rows: rows
+
+    puts table
+  end
+
+  def headings(array)
+    headings = []
+
+    array.each do |hash|
+      headings = headings | hash.keys
+    end
+
+    headings
   end
 end
