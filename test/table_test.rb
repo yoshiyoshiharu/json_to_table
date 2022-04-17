f8bb0ac2888af6a120aa88dd859857b9d1510df0
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2022-04-17 23:31:54 +0900

    headingsメソッドのテスト

--- test/table_test.rb
+++ test/table_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
-class JsonToTableTest < Minitest::Test
+class TableTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::JsonToTable::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_headings
+    users = [
+      { id: 1, name: 'TestUser1' },
+      { id: 2, name: 'TestUser2' },
+      { id: 3, name: 'TestUser3', email: 'test@test.com' }
+    ]
+    table = JsonToTable::Table.new(users)
+    
+    assert [:id, :name, :email], table.send(:headings)
   end
 end
