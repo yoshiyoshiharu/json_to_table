0f133c3b4e216b23ea67493d41ae24369e28ae50
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2022-04-23 16:08:32 +0900

    rowsメソッドのテスト

--- test/table_test.rb
+++ test/table_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require_relative "test_helper"
 
 class TableTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -14,5 +14,22 @@ class TableTest < Minitest::Test
     table = JsonToTable::Table.new(users)
     
     assert [:id, :name, :email], table.send(:headings)
+  end
+
+  def test_rows
+    users = [
+      { id: 1, name: 'TestUser1' },
+      { id: 2, name: 'TestUser2' },
+      { id: 3, name: 'TestUser3', email: 'test@test.com' }
+    ]
+
+    expected_array = [
+      [1, 'TestUser1', nil],
+      [2, 'TestUser2', nil],
+      [3, 'TestUser3', 'test@test.com'],
+    ]
+    table = JsonToTable::Table.new(users)
+
+    assert_equal expected_array, table.send(:rows)
   end
 end
